723fdb4c631a2d1da81b3f6cd305d383b2706ce1
Ezequiel Izquierdo <ezizquierdo@santandertecnologia.com.ar>
2025-02-06 11:35:19 -0300

    Actualizacion clase 3

--- Clase 01-27/CLASE_01-27-25/5-ACTIVIDAD-4.js
+++ Clase 01-27/CLASE_01-27-25/5-ACTIVIDAD-4.js
@@ -3,23 +3,23 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-async function main() {
+async function main() { // Función principal
   try {
     console.log(__dirname) // ~/OneDrive/Escritorio/CODERHOUSE/75240-CLASE/CLASE_01-27-25/package.json
-    const packagePath = path.join(__dirname, "package.json");
+    const packagePath = path.join(__dirname, "package.json"); // dirname es el directorio donde se encuentra el archivo que se está ejecutando en este momento (en este caso, el archivo 5-ACTIVIDAD-4.js) 
 
-    const packageContent = await fs.readFile(packagePath, "utf8");
+    const packageContent = await fs.readFile(packagePath, "utf8"); // Lee el archivo package.json y lo guarda en la variable packageContent
 
     // Paso 2: Crear el objeto info
-    const info = {
+    const info = { // Objeto info
       contenidoStr: packageContent, // Contenido del archivo leído en formato string
-      contenidoObj: JSON.parse(packageContent), // Contenido del archivo leído en formato objeto
-      size: Buffer.byteLength(packageContent, "utf8"), // Tamaño en bytes del archivo
+      contenidoObj: JSON.parse(packageContent), // Contenido del archivo leído en formato objeto (parseado)
+      size: Buffer.byteLength(packageContent, "utf8"), // Tamaño en bytes del archivo leído
     };
 
     // Paso 3: Mostrar el objeto info por consola
-    console.log("Objeto info:");
-    console.log(info);
+    console.log("Objeto info:"); // Imprime "Objeto info:"
+    console.log(info); // Imprime el objeto info que es el contenido del archivo package.json en formato string, objeto y el tamaño en bytes
     
   } catch (error) {
     throw new Error(`Error al procesar archivo: ${error.message}`);
